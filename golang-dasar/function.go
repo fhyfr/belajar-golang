1c71e7b9171bf3cb3d516120f4f7b621a55d2cae
Firman <fhaya.firman@gmail.com>
2021-08-30 16:13:01 +0700

    update function parameter, function return value

--- golang-dasar/function.go
+++ golang-dasar/function.go
@@ -6,8 +6,22 @@ func sayHello() {
 	fmt.Println("Halo Dunia")
 }
 
+// function dengan parameter
+func sayHelloTo(firstName string, lastName string) {
+	fmt.Println("Hello", firstName, lastName)
+}
+
+// function return value
+func getNumber(number int) int {
+	return number
+}
+
 func main() {
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 3; i++ {
 		sayHello()
 	}
+
+	// memanggil function dengan argument
+	sayHelloTo("Firman", "sah")
+	fmt.Println(getNumber(100))
 }
