46ba53e7c224a2e8f74c6e29ef640ff042b0fe09
Firman <fhaya.firman@gmail.com>
2021-08-31 11:35:31 +0700

    add return multiple value, named return value

--- golang-dasar/function.go
+++ golang-dasar/function.go
@@ -16,6 +16,20 @@ func getNumber(number int) int {
 	return number
 }
 
+// function return multiple value
+func getBiodata() (string, string, int) {
+	return "Firman", "Sah", 22
+}
+
+// named return value
+func getCompleteName() (firstName, middleName, lastName string) {
+	firstName = "Firman"
+	middleName = "Syah"
+	lastName = "Fhaya"
+
+	return
+}
+
 func main() {
 	for i := 0; i < 3; i++ {
 		sayHello()
@@ -24,4 +38,9 @@ func main() {
 	// memanggil function dengan argument
 	sayHelloTo("Firman", "sah")
 	fmt.Println(getNumber(100))
+
+	firstName, _, _ := getBiodata()
+
+	fmt.Println(firstName)
+	fmt.Println(getCompleteName())
 }
