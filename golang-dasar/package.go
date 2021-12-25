2c8c1e775274216db6886113ccac9f2cf64b9f88
fhyfr28 <firmansah.firmansah@kumparan.com>
2021-12-25 20:12:25 +0700

    package strconv

--- golang-dasar/package.go
+++ golang-dasar/package.go
@@ -3,10 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	// package flag
+	/* package flag */
 	var host *string = flag.String("host", "localhost", "put your database host")
 	var user *string = flag.String("user", "firman", "put your database user")
 	var password *string = flag.String("password", "firmanganteng123", "put your database password")
@@ -21,4 +22,31 @@ func main() {
 
 	// package flag: usage
 	// ex: go run golang-dasar/package.go -host=https://hostname.com -user=root -password=root123456789 -pool=2
+
+	/* package strconv / string conversion */
+	boolean, err := strconv.ParseBool("t")
+
+	if err == nil {
+		fmt.Println(boolean)
+	} else {
+		fmt.Println(err.Error())
+	}
+
+	number, err := strconv.ParseInt("245000", 10, 64)
+
+	if err == nil {
+		fmt.Println(number)
+	} else {
+		fmt.Println(err.Error())
+	}
+
+	// convert string to int with decimal base (automatic)
+	// alternate of ParseInt
+	value, err := strconv.Atoi("10231313")
+
+	if err == nil {
+		fmt.Println(value)
+	} else {
+		fmt.Println(err.Error())
+	}
 }
