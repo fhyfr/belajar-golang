86127343b1b39ad2adbe97c45403f42915f7cf1b
fhyfr28 <firmansah.firmansah@kumparan.com>
2022-01-02 22:34:28 +0700

    throw error unit test

--- unit-test/helper/hello_world_test.go
+++ unit-test/helper/hello_world_test.go
@@ -1,14 +1,29 @@
 package helper
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Firman")
 
-	if result != "HelloFirman" {
-		// error 
-		Fail("Result is not Hello Firman")
+	if result != "Hello Firman" {
+		// error from unit test
+		t.Error("Result must be 'Hello Firman'")
 	}
+
+	fmt.Println("Testing Done")
 }
 
 // command to running unit test in all package : go test ./...
+
+/*
+
+Failed Error: 
+- t.Fail()
+- t.FailNow()
+- t.Error(args...)
+- t.Fatal(args...)
+
+*/
